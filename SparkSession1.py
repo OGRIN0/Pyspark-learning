db7d17bca41b51d69f7574d42170b62148d27d26
AMARNATH <142997781+OGRIN0@users.noreply.github.com>
2024-11-12 20:01:08 +0530

    Update SparkSession1.py

--- SparkSession1.py
+++ SparkSession1.py
@@ -12,6 +12,3 @@ spark = SparkSession.builder \
     .appName("PySpark-Get-Started") \
     .getOrCreate()
 
-data = [("Alice", 25), ("Bob", 30), ("Charlie", 35)]
-df = spark.createDataFrame(data, ["Name", "Age"])
-df.show()
